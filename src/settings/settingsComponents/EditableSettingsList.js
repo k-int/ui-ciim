ef975657dd88bcac8cdfe3ad9c392544c6e99203
Ethan Freestone <ethan.freestone@k-int.co.uk>
2020-12-10 15:23:44 +0000

    added translations and settings pages

---
+++ src/settings/settingsComponents/EditableSettingsList.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FieldArray } from 'react-final-form-arrays';
+import { Form } from 'react-final-form';
+import arrayMutators from 'final-form-arrays';
+
+import { Pane } from '@folio/stripes/components';
+import { FormattedMessage } from 'react-intl';
+
+import EditableSettingsListFieldArray from './EditableSettingsListFieldArray';
+
+class EditableSettingsList extends React.Component {
+  static propTypes = {
+    onSave: PropTypes.func,
+    data: PropTypes.shape({
+      refdatavalues: PropTypes.arrayOf(PropTypes.object)
+    }),
+    settingSection: PropTypes.string,
+    initialValues: PropTypes.object
+  };
+
+  handleSave = (...rest) => {
+    return this.props.onSave(...rest);
+  }
+
+  render() {
+    const {
+      data,
+      settingSection,
+      initialValues,
+    } = this.props;
+    return (
+      <Form
+        onSubmit={this.handleSave}
+        initialValues={initialValues}
+        enableReinitialize
+        keepDirtyOnReinitialize
+        mutators={{
+          setSettingValue: (args, state, tools) => {
+            tools.changeValue(state, args[0], () => args[1]);
+          },
+          ...arrayMutators
+        }}
+        subscription={{ value: true }}
+        navigationCheck
+      >
+        {({ handleSubmit, mutators }) => (
+          <Pane
+            defaultWidth="fill"
+            id={`settings-${settingSection}`}
+            paneTitle={<FormattedMessage id={`ui-rs.settingsSection.${settingSection}`} />}
+          >
+            <form onSubmit={handleSubmit}>
+              <FieldArray
+                component={EditableSettingsListFieldArray}
+                name="settings"
+                onSave={this.handleSave}
+                mutators={mutators}
+                data={{
+                  refdatavalues: data?.refdatavalues
+                }}
+                initialValues={initialValues}
+              />
+            </form>
+          </Pane>
+        )}
+      </Form>
+    );
+  }
+}
+
+export default EditableSettingsList;
