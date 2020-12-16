100bc66bc407f1b1bad3d6529beda62188674dac
Ethan Freestone <ethan.freestone@k-int.co.uk>
2020-12-16 13:42:05 +0000

    closable filter pane

--- src/components/CiimView/CiimView.js
+++ src/components/CiimView/CiimView.js
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FilterPaneSearch, Pane, PaneHeader, PaneMenu, Paneset } from '@folio/stripes/components';
+
+import {
+  CollapseFilterPaneButton,
+  ExpandFilterPaneButton,
+} from '@folio/stripes/smart-components';
+
+import { FormattedMessage } from 'react-intl';
+
+import css from './CiimView.css';
 
 export default function CiimView() {
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showFilter, setShowFilter] = useState(true);
+
+  const toggleFilterPane = () => {
+    setShowFilter(!showFilter);
+  }
+
+  const SearchHeader = () => (
+    <PaneHeader
+      firstMenu={
+        !showFilter &&
+          <PaneMenu>
+            <ExpandFilterPaneButton
+              onClick={toggleFilterPane}
+            />
+          </PaneMenu>
+      }
+    >
+      <FilterPaneSearch
+        id="SearchField"
+        onChange={e => setSearchTerm(e.target)}
+        onClear={setSearchTerm("")}
+        value={searchTerm}
+        searchAriaLabel="Search"
+      />
+    </PaneHeader>
+  );
+
   return (
-    <p> This is where the CIIM app will go </p>
+    <Paneset>
+      {showFilter &&
+        <Pane
+          defaultWidth="20%"
+          paneTitle={<FormattedMessage id="ui-ciim.ciimView.paneHeader.filter" />}
+          lastMenu={
+            <PaneMenu>
+              <CollapseFilterPaneButton onClick={toggleFilterPane} />
+            </PaneMenu>
+          }
+        >
+          <p> This is where the CIIM filters will go </p>
+        </Pane>
+      }
+      <Pane
+        defaultWidth="fill"
+        renderHeader={SearchHeader}
+      >
+        <p> This is where the CIIM app will go </p>
+      </Pane>
+    </Paneset>
   );
 }
