92ccd1420908f33eb5c7543db1ce02646fb1e24e
Ethan Freestone <ethan.freestone@k-int.co.uk>
2020-12-17 14:44:59 +0000

    added dummy filters

--- src/components/CiimFilters/CiimFilters.js
+++ src/components/CiimFilters/CiimFilters.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import {
+  Button,
+  Row
+} from '@folio/stripes/components';
+
+import { AppIcon } from '@folio/stripes/core';
+import { FormattedMessage } from 'react-intl';
+
+const Source = ({ appIconProps, name }) => {
+  const [selected, setSelected] = useState(false);
+  const onClick = () => {
+    setSelected(!selected)
+  }
+  return (
+    <Row
+      key={`source_${name}`}
+    >
+      <Button
+        buttonStyle={null}
+        onClick={onClick}
+      >
+        <AppIcon {...appIconProps}>
+          <FormattedMessage id={`ui-ciim.ciimFilters.filter.${name}`} />
+          {selected ? " (selected)" : " (not selected)"}
+        </AppIcon>
+      </Button>
+    </Row>
+  );
+}
 
 export default function CiimFilters() {
+  const sourceList = [
+    <Source
+      appIconProps={{app: "agreements", size: "small"}}
+      name="agreements"
+    />,
+    <Source
+      appIconProps={{app: "agreements", iconKey:"eresource", size: "small"}}
+      name="eResources"
+    />,
+    <Source
+      appIconProps={{app: "inventory", iconKey:"instance", size: "small"}}
+      name="inventoryItems"
+    />,
+    <Source
+      appIconProps={{app: "orders", size: "small"}}
+      name="purchaseOrders"
+    />,
+  ];
+
   return (
-    <p> This is where the CIIM filters will go </p>
+    sourceList
   );
 }
--- src/routes/CiimRoute.js
+++ src/routes/CiimRoute.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { stripesConnect } from '@folio/stripes/core';
 
 import CiimView from '../components/CiimView';
 
-export default stripesConnect(() => {
+const CiimRoute = () => {
+  const [filters, setFilters] = useState({})
+  const handleFilterChange = (incomingFilters) => {
+    setFilters(incomingFilters)
+  }
+
+  console.log("Filters: %o", Object.keys(filters) !== 0 ? filters : "No filter set")
+
   return (
     <CiimView />
   );
-});
+};
+
+export default stripesConnect(CiimRoute);
