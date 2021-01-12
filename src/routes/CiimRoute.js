b25285abdc57ea6e81f66f25f6df364123555d78
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2021-01-12 12:14:40 +0000

    Filter and query parsing (#6)
    * filter state now stored in URL, with correct parsing/deparsing
    
    * added query term to url state management
    
    * forgot to git add files

--- src/routes/CiimRoute.js
+++ src/routes/CiimRoute.js
@@ -1,13 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { stripesConnect } from '@folio/stripes/core';
+import {
+  buildFilterString,
+  locationQuerySetter,
+  locationQueryGetter,
+  filterStringToObject
+} from '../util/filterUtils';
 
 import CiimView from '../components/CiimView';
 
-const CiimRoute = () => {
-  const [filters, setFilters] = useState({});
+const propTypes = {
+  location: PropTypes.object,
+  history: PropTypes.object
+};
+
+const CiimRoute = ({ location, history }) => {
   const handleFilterChange = (incomingFilters) => {
-    setFilters(incomingFilters);
+    const filterString = buildFilterString(incomingFilters);
+    locationQuerySetter({ location, history, nsValues: { filters: filterString } });
   };
+
+  const handleSearchTermChange = (incomingSearchTerm) => {
+    locationQuerySetter({ location, history, nsValues: { query: incomingSearchTerm } });
+  };
+
+  const parseFilters = () => {
+    const query = locationQueryGetter({ location });
+    const parsedFilters = filterStringToObject(query.filters);
+    return parsedFilters;
+  };
+
+  const parsedQuery = locationQueryGetter({ location })?.query;
 
   const filterValues = {
     resourceType: [
@@ -22,17 +46,19 @@ const CiimRoute = () => {
     ]
   };
 
-  console.log('Filters: %o', Object.keys(filters) !== 0 ? filters : 'No filter set');
-
   return (
     <CiimView
       filterData={{
-        currentFilters: filters,
+        currentFilters: parseFilters(),
         filterValues,
-        onFilterChange: handleFilterChange
+        onFilterChange: handleFilterChange,
+        onSearchChange: handleSearchTermChange,
+        searchTerm: parsedQuery
       }}
     />
   );
 };
+
+CiimRoute.propTypes = propTypes;
 
 export default stripesConnect(CiimRoute);
